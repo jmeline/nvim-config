d9f5a3e1d1a0027ccff78830ab4aff985d961723
Jacob Meline <jacob.meline@gmail.com>
2022-03-24 00:52:11 -0600

    Remove extra comments from colorscheme.lua

--- nvim/.config/nvim/lua/core/colorscheme.lua
+++ nvim/.config/nvim/lua/core/colorscheme.lua
@@ -3,16 +3,5 @@ local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
 
 local dracula_pro_vim = os.getenv("DRACULA_PRO_VIM_DIR")
 
--- print(dracula_pro_vim)
---
--- local theme = "dracula_pro_buffy"
---
--- if not dracula_pro_vim then
---   theme = "dracula"
--- end
-
--- print(theme)
-
--- load up personal colorscheme --
 opt.runtimepath:append(dracula_pro_vim)
 cmd "colorscheme dracula_pro_buffy"
