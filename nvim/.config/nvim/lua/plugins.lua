d97569de58adb9b9d157ee01c45408fb5dcb1675
Jacob Meline <jacob.meline@gmail.com>
2023-01-24 22:04:13 -0700

    Add nvim tmux navigation

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -92,6 +92,20 @@ return require('packer').startup(function(use)
       -- in the form "LspDiagnosticsSignWarning"
     end
   }
+  use { 'alexghergh/nvim-tmux-navigation', config = function()
+    require'nvim-tmux-navigation'.setup {
+      disable_when_zoomed = true, -- defaults to false
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        next = "<C-Space>",
+      }
+    }
+    end
+  }
 
   --'tomtom/tcomment_vim'
   -- 'tpope/vim-commentary'
