571ed2829e0267f195e1299d1fcbc29d735903b5
Jacob Meline <jacob.meline@gmail.com>
2023-01-30 00:48:59 -0700

    Move auto install packer code into an extensions file.

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,18 +1,6 @@
 local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
 local execute = vim.api.nvim_command
-
--- Auto install packer if it does not exist
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
+local ensure_packer = require("util.plugin").ensure_packer
 
 -- returns the require for use in `config` parameter of packer's use
 -- expects the name of the config file
