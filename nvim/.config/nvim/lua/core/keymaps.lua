a468ce8a26577ccb92d169c8245208637fac9b61
Jacob Meline <jacob.meline@gmail.com>
2023-08-05 22:04:32 -0600

    Remove packer sync from nvim config

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -48,11 +48,6 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-------------
--- prevents --
-------------
-map("n", "<leader>ps", ":PackerSync<CR>")
-
 ---------------
 -- Nvim Tree --
 ---------------
