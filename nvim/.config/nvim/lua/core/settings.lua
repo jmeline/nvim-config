8401b185ecda65bffc8339ed53294a262e94e438
Jacob Meline <jacob.meline@drivetime.com>
2023-08-30 09:53:59 -0600

    Disable built-in unnecessary nvim plugins

--- nvim/.config/nvim/lua/core/settings.lua
+++ nvim/.config/nvim/lua/core/settings.lua
@@ -134,27 +134,27 @@ cmd [[
 opt.shortmess:append "sI"
 
 -- Disable builtins plugins
--- local disabled_built_ins = {
---     "netrw",
---     "netrwPlugin",
---     "netrwSettings",
---     "netrwFileHandlers",
---     "gzip",
---     "zip",
---     "zipPlugin",
---     "tar",
---     "tarPlugin",
---     "getscript",
---     "getscriptPlugin",
---     "vimball",
---     "vimballPlugin",
---     "2html_plugin",
---     "logipat",
---     "rrhelper",
---     "spellfile_plugin",
---     "matchit"
--- }
---
--- for _, plugin in pairs(disabled_built_ins) do
---     g["loaded_" .. plugin] = 1
--- end
+local disabled_built_ins = {
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "gzip",
+    "zip",
+    "zipPlugin",
+    "tar",
+    "tarPlugin",
+    "getscript",
+    "getscriptPlugin",
+    "vimball",
+    "vimballPlugin",
+    "2html_plugin",
+    "logipat",
+    "rrhelper",
+    "spellfile_plugin",
+    "matchit"
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+    g["loaded_" .. plugin] = 1
+end
