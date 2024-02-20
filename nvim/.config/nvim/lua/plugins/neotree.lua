7c79377ad37cded9945f675c423aec13be7bd74f
Jacob Meline <jacob.meline@drivetime.com>
2024-02-20 14:20:30 -0700

    Add neotree reveal hotkey

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,13 +1,14 @@
 return
 {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-    keys = {
-      {"<leader>n", "<cmd>Neotree toggle<cr>", desc = "NeoTree"},
-    }
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  keys = {
+    {"<leader>n", "<cmd>Neotree toggle<cr>", desc = "NeoTree"},
+    {"<leader>r", "<cmd>Neotree reveal<cr>", desc = "NeoTree"},
+  }
 }
