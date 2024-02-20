75fcf8f33340f3cfaca0a9a43aeeac5073824b28
Jacob Meline <jacob.meline@drivetime.com>
2024-02-20 10:19:18 -0700

    Clean up old lsp-zero configs

--- nvim/.config/nvim/after/plugin/lsp-zero.lua
+++ nvim/.config/nvim/after/plugin/lsp-zero.lua
@@ -81,44 +81,6 @@ cmp.setup({
   })
 })
 
--- local cmp_select_opts = {behavior = cmp.SelectBehavior.Select}
--- local cmp_mappings = lsp_zero.defaults.cmp_mappings({
---   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select_opts),
---   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select_opts),
---   ['<C-y>'] = cmp.mapping.confirm({select = true}),
---   ['<CR>'] = cmp.mapping.confirm({select = false}),
---   ["<C-Space>"] = cmp.mapping.complete(),
---
---   ['<Tab>'] = cmp.mapping(function(fallback)
---     local col = vim.fn.col('.') - 1
---
---     if cmp.visible() then
---       cmp.select_next_item(cmp_select_opts)
---     elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
---       fallback()
---     else
---       cmp.complete()
---     end
---   end, {'i', 's'}),
---
---   ['<S-Tab>'] = cmp.mapping(function(fallback)
---     if cmp.visible() then
---       cmp.select_prev_item(cmp_select_opts)
---     else
---       fallback()
---     end
---   end, {'i', 's'}),
--- })
-
--- disable completion with tab
--- this helps with copilot setup
--- cmp_mappings['<Tab>'] = nil
--- cmp_mappings['<S-Tab>'] = nil
-
--- lsp_zero.setup_nvim_cmp({
---   mapping = cmp_mappings
--- })
-
 lsp_zero.set_preferences({
   suggest_lsp_servers = true,
   sign_icons = {
@@ -128,8 +90,5 @@ lsp_zero.set_preferences({
     info = ''
   }
 })
-
--- (Optional) Configure lua language server for neovim
--- lsp.nvim_workspace()
 
 lsp_zero.setup()
