a023dc1cf7b486c05db45439f86e841043731023
Jacob Meline <jacob.meline@drivetime.com>
2024-02-25 18:15:44 -0700

    Remove a couple settings from obsidian

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -32,12 +32,17 @@ return
     { "<leader>os", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick Switch", mode = "n" },
     { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Show location list of backlinks", mode = "n" },
     { "<leader>of", "<cmd>ObsidianFollowLink<cr>", desc = "Follow link under cursor", mode = "n" },
+    { "<leader>ow", "<cmd>ObsidianWorkspaces<cr>", desc = "Change Obsidian Workspaces", mode = "n" },
   },
   opts = {
     workspaces = {
       {
         name = "personal",
         path = "~/code/vault/personal",
+      },
+      {
+        name = "drivetime",
+        path = "~/code/vault/drivetime",
       }
     },
 
@@ -78,22 +83,22 @@ return
     },
 
     -- Optional, customize how names/IDs for new notes are created.
-    note_id_func = function(title)
-      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-      -- In this case a note with the title 'My new note' will be given an ID that looks
-      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-      local suffix = ""
-      if title ~= nil then
-        -- If title is given, transform it into valid file name.
-        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-      else
-        -- If title is nil, just add 4 random uppercase letters to the suffix.
-        for _ = 1, 4 do
-          suffix = suffix .. string.char(math.random(65, 90))
-        end
-      end
-      return tostring(os.time()) .. "-" .. suffix
-    end,
+    -- note_id_func = function(title)
+    --   -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    --   -- In this case a note with the title 'My new note' will be given an ID that looks
+    --   -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+    --   local suffix = ""
+    --   if title ~= nil then
+    --     -- If title is given, transform it into valid file name.
+    --     suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    --   else
+    --     -- If title is nil, just add 4 random uppercase letters to the suffix.
+    --     for _ = 1, 4 do
+    --       suffix = suffix .. string.char(math.random(65, 90))
+    --     end
+    --   end
+    --   return tostring(os.time()) .. "-" .. suffix
+    -- end,
 
     -- Optional, customize how wiki links are formatted.
     ---@param opts {path: string, label: string, id: string|?}
