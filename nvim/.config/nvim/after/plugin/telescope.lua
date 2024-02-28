89775a10f82352ad093727410738d2be5786fe78
Jacob Meline <jacob.meline@gmail.com>
2024-02-27 23:54:43 -0700

    Update telescope plugin to use fzf native
    Create neovim config search directly within telescope

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,28 @@
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- vim.keymap.set('n', '<C-p>', builtin.git_file, {})
+
+-- customize telescope to jump to nvim config from anywhere
+local _edit_neovim = function()
+  builtin.git_files {
+    shorten_path = true,
+    cwd = "~/.config/nvim",
+    prompt = "~ dotfiles ~",
+    height = 10,
+    layout_strategy = 'horizontal',
+    layout_options = {
+      preview_width = 0.75,
+    }
+  }
+end
+
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>ft', builtin.colorscheme, {})
+vim.keymap.set('n', '<leader>fp', builtin.planets, {})
+
+vim.keymap.set('n', '<leader>en', _edit_neovim, {})
+
--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -1,7 +1,6 @@
 
 return
 {
-  {'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = {{'nvim-lua/plenary.nvim'}}},
   {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
   {'nvim-treesitter/playground'},
   {'akinsho/toggleterm.nvim'},
